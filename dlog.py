39e0b5f137c089068d5f9c30d59be832b8e71105
Robert <rhenderson@wayne.edu>
2014-05-28 12:02:45 -0400

    added argument parse with add and init flags

--- dlog.py
+++ dlog.py
@@ -6,9 +6,17 @@ try:
     with open('dlog.json') as file:
         pass
 except IOError as e:
-    print("Unable to open file")
+    print('Unable to find your Daily Log file. Use the --init flag to create a new one.')
 
+parser = argparse.ArgumentParser(description='Daily Log App')
 
+parser.add_argument('--init', action="store_true", dest='initialization_switch', default=False)
+parser.add_argument('-a', action='append', dest='collection', help='Add n-many projects to your daily log. For example: -a book -a "book review" -a "journal article"')
+
+results = parser.parse_args()
+
+print('initialization_switch=', results.initialization_switch)
+print('collection=', results.collection)
 
 # Some example json
 # {
