10d8c42e4a4d59aa038e80871d4dfbd2e2925cf8
balamkej <balamkej@gmail.com>
2014-06-03 19:16:43 -0400

    Hooked command line arguments into core functions

--- dlog.py
+++ dlog.py
@@ -12,11 +12,11 @@ parser = argparse.ArgumentParser(description='Daily Log App')
 
 group = parser.add_mutually_exclusive_group()
 
-# Define a series of flags and assign them to a mutually exclusive group: 
+# Define a series of flags and assign them to a mutually exclusive group:
 # --init stores True in var init_switch, else False
 # -i appends an int to the list in var increment. We allow multiple -i flags, e.g., -i book -i "book review" -i "journal article"
 # -a appends a string to the list in var project. We allow multiple -a flags, e.g., -a book -a "book review" -a "journal article"
-# -s stores exactly two int arguments as list in var swap 
+# -s stores exactly two int arguments as list in var swap
 
 group.add_argument('--init', action="store_true", dest='init_switch', default=False)
 group.add_argument('-i', action='append', dest='increment', type=int)
@@ -24,11 +24,6 @@ group.add_argument('-a', action='append', dest='projects')
 group.add_argument('-s', nargs=2, action='store', dest='swap', type=int)
 
 results = parser.parse_args()
-
-print('init_switch=', results.init_switch)
-print('increment=', results.increment)
-print('projects=', results.projects)
-print('swap=', results.swap)
 
 # Touch dlog.json and write to it a json object
 def init():
@@ -50,7 +45,7 @@ def add(title):
     if title in titles:
         print('You already have the project called \"%s\" in your Daily Log.' % (title))
     else:
-        data['projects'].append({ "title":title , "count":0 })   
+        data['projects'].append({ "title":title , "count":0 })
         f = open('dlog.json', 'w')
         json.dump(data, f)
         f.close()
@@ -67,9 +62,9 @@ def increment(index):
     if data_len == 0:
         print('You don\'t have any projects to increment.')
     elif data_len <= index or index < 0:
-        print('Index is out of range.') 
+        print('Index is out of range.')
     else:
-        data['projects'][index]['count'] = data['projects'][index]['count'] + 1 
+        data['projects'][index]['count'] = data['projects'][index]['count'] + 1
         f = open('dlog.json', 'w')
         json.dump(data, f)
         f.close()
@@ -119,11 +114,27 @@ def delete(index):
         f.close()
 
 
+# IF-block calls appropriate functions on command line arguments supplied by flags. Given no flags, print Daily Log graph.
+if results.init_switch == True:
+    init() # Run init() if --init flag is supplied
+elif results.increment != None:
+    for i in results.increment:
+        increment(i) # Call increment() on each index supplied by -i flag
+elif results.projects != None:
+    for i in results.projects:
+        add(i) # Call add() on each index supplied by -a flag
+elif results.swap != None:
+    first = results.swap[0]
+    second = results.swap[1]
+    swap(first,second) # Call swap() on the two argument supplied by -s flag. Note that I have to retrieve them from a list. This could be refactored.
+else:
+    print('Print pretty graph.')
+
 # Some example json
 # {
 # "projects": [
-# { "title":"JoS Review" , "count":14 }, 
-# { "title":"Mayan semantics review article" , "count":40 }, 
+# { "title":"JoS Review" , "count":14 },
+# { "title":"Mayan semantics review article" , "count":40 },
 # { "title":"Swarms" , "count":3 }
 # ]
 # }
